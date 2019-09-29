a3d8335d4bf908b036eda9ff97475741d4ba690a
Rob Allen <rob@akrabat.com>
2019-09-29 23:55:41 +0100

    Add ProblemDetailsErrorHandler
    This allows us to set the correct Problem Details content type

--- config/middleware.php
+++ config/middleware.php
@@ -2,17 +2,15 @@
 
 declare(strict_types=1);
 
-use App\Error\Renderer\JsonProblemDetailsErrorRenderer;
-use App\Error\Renderer\XmlProblemDetailsErrorRenderer;
+use App\Handler\ProblemDetailsErrorHandler;
 use Slim\App;
 
 return static function (App $app) {
     $app->addRoutingMiddleware();
     $app->addBodyParsingMiddleware();
     $errorMiddleware = $app->addErrorMiddleware(true, true, true);
-
-    // Register new error renderers
-    $errorHandler = $errorMiddleware->getDefaultErrorHandler();
-    $errorHandler->registerErrorRenderer('application/json', JsonProblemDetailsErrorRenderer::class);
-    $errorHandler->registerErrorRenderer('application/xml', XmlProblemDetailsErrorRenderer::class);
+    $errorMiddleware->setDefaultErrorHandler(new ProblemDetailsErrorHandler(
+        $app->getCallableResolver(),
+        $app->getResponseFactory()
+    ));
 };
---
+++ src/Handler/ProblemDetailsErrorHandler.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Handler;
+
+use App\Error\Renderer\JsonProblemDetailsErrorRenderer;
+use App\Error\Renderer\XmlProblemDetailsErrorRenderer;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ResponseInterface;
+use Slim\Exception\HttpMethodNotAllowedException;
+use Slim\Handlers\ErrorHandler as SlimErrorHandler;
+use Slim\Interfaces\CallableResolverInterface;
+
+final class ProblemDetailsErrorHandler extends SlimErrorHandler
+{
+    public function __construct(CallableResolverInterface $callableResolver, ResponseFactoryInterface $responseFactory)
+    {
+        parent::__construct($callableResolver, $responseFactory);
+
+        $this->errorRenderers['application/json'] = JsonProblemDetailsErrorRenderer::class;
+        $this->errorRenderers['application/xml'] = XmlProblemDetailsErrorRenderer::class;
+    }
+
+    /**
+     * @return ResponseInterface
+     */
+    protected function respond(): ResponseInterface
+    {
+        $response = $this->responseFactory->createResponse($this->statusCode);
+        if ($this->contentType !== null && array_key_exists($this->contentType, $this->errorRenderers)) {
+            $contentType = $this->contentType;
+            if (stripos($this->contentType, 'json') !== false) {
+                $contentType = 'application/problem+json';
+            } elseif (stripos($this->contentType, 'xml') !== false) {
+                $contentType = 'application/problem+xml';
+            }
+            $response = $response->withHeader('Content-type', $contentType);
+        } else {
+            $response = $response->withHeader('Content-type', $this->defaultErrorRendererContentType);
+        }
+
+        if ($this->exception instanceof HttpMethodNotAllowedException) {
+            $allowedMethods = implode(', ', $this->exception->getAllowedMethods());
+            $response = $response->withHeader('Allow', $allowedMethods);
+        }
+
+        $renderer = $this->determineRenderer();
+        $body = call_user_func($renderer, $this->exception, $this->displayErrorDetails);
+        $response->getBody()->write($body);
+
+        return $response;
+    }
+}
