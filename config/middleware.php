14cafd3e3965f9514db542b2e2126bdee39f398e
Rob Allen <rob@akrabat.com>
2019-10-03 22:29:40 +0100

    Add TimerMiddleware
    This is an example of middleware in the project.

--- config/middleware.php
+++ config/middleware.php
@@ -3,9 +3,11 @@
 declare(strict_types=1);
 
 use App\Handler\ProblemDetailsErrorHandler;
+use App\Middleware\TimerMiddleware;
 use Slim\App;
 
 return static function (App $app) {
+    $app->add(TimerMiddleware::class);
     $app->addRoutingMiddleware();
     $app->addBodyParsingMiddleware();
     $errorMiddleware = $app->addErrorMiddleware(true, true, true);
---
+++ src/Middleware/TimerMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Middleware;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class TimerMiddleware implements MiddlewareInterface
+{
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $start = microtime(true);
+
+        $response = $handler->handle($request);
+
+        $taken = microtime(true) - $start;
+
+        return $response->withHeader('Time-Taken', $taken);
+    }
+}
