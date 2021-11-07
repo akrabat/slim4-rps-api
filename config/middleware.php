a714eb6f3b33c10e0492a1789d83b58f09cf931b
Rob Allen <rob@akrabat.com>
2021-11-07 12:29:24 +0000

    Minor clarity tweak

--- config/middleware.php
+++ config/middleware.php
@@ -9,12 +9,12 @@ use Slim\App;
 
 return static function (App $app) {
     // LIFO stack
-    $errorMiddleware = $app->addErrorMiddleware(true, true, true);
     $app->add(TimerMiddleware::class);
     $app->add(BaseUrlMiddleware::class);
     $app->addBodyParsingMiddleware();
     $app->addRoutingMiddleware();
 
+    $errorMiddleware = $app->addErrorMiddleware(true, true, true);
     $errorMiddleware->setDefaultErrorHandler(new ProblemDetailsErrorHandler(
         $app->getCallableResolver(),
         $app->getResponseFactory()
